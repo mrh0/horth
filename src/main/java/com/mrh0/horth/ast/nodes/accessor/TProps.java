7f5160e5fad3e91ae1361c832e28305ca6398b90
MRH0 <mrhmail1@gmail.com>
2022-02-21 00:43:24 +0100

    Create TProps.java

---
+++ src/main/java/com/mrh0/horth/ast/nodes/accessor/TProps.java
@@ -0,0 +1,26 @@
+package com.mrh0.horth.ast.nodes.accessor;
+
+import com.mrh0.horth.ast.nodes.Tok;
+import com.mrh0.horth.exceptions.HorthException;
+import com.mrh0.horth.instructions.high.HighInst;
+
+import java.util.List;
+
+public class TProps extends Tok {
+    private final List<String> props;
+
+    public TProps(List<String> props) {
+        this.props = props;
+    }
+
+    @Override
+    public StringBuilder toString(StringBuilder sb) {
+        sb.append(".");
+        return sb;
+    }
+
+    @Override
+    public void expand(List<HighInst> space) throws HorthException {
+
+    }
+}
